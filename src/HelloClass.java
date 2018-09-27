abb5e54da12f730bbd5e4771f43616b974f0bd37
kim test <kim@example.com>
2018-09-28 08:52:57 +0900

    Initial commit

---
+++ src/HelloClass.java
@@ -0,0 +1,6 @@
+public class HelloClass {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello Project!!");
+        System.out.println("aaaaaaaa");
+    }
+}
