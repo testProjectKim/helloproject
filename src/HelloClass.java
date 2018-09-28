f6d74e707d0a999d03f6a92fc98f6e3339280c4f
kim test <kim@example.com>
2018-09-28 09:15:06 +0900

    commit test!!

--- src/HelloClass.java
+++ src/HelloClass.java
@@ -1,6 +1,9 @@
 public class HelloClass {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args){
         System.out.println("Hello Project!!");
         System.out.println("aaaaaaaa");
+
+        System.out.println("Hello Project222!!");
+        System.out.println("bbbbb");
     }
 }
